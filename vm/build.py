395752b1e9dc359b8e11352328aa646f33a3c9de
Saurabh <saurabh6790@gmail.com>
2017-10-04 17:45:19 +0530

    build vm with common installer

--- vm/build.py
+++ vm/build.py
@@ -1,47 +1,45 @@
+#!/usr/bin/python
+
 """
 Builds a vm and puts it in ~/public with a latest.json that has its filename and md5sum
 """
 import os
 import subprocess
-import json
 import stat
 
-OUTPUT_DIR = 'output-virtualbox-ovf'
+BUILD_DIR = os.path.join(os.path.expanduser('~'), 'bench-develop', 'vm')
+OUTPUT_DIR = os.path.join(BUILD_DIR, 'output-virtualbox-ovf')
 PUBLIC_DIR = os.path.join(os.path.expanduser('~'),  'public')
+BUILD_LIST = [os.path.join(BUILD_DIR, "vm.json"), os.path.join(BUILD_DIR, "vm-develop.json")]
 
 def main():
-	build_vm()
-	update_latest()
-	move_to_public()
-	cleanup()
+	for vm in BUILD_LIST:
+		build_vm(vm)
+		move_to_public()
+		cleanup()
+	generate_checksums()
 
-def build_vm():
-	subprocess.check_call("./packer build vm.json", shell=True)
-
+def build_vm(vm):
+        subprocess.check_call("/home/frappe/bench-develop/vm/packer build {vm}".format(vm=vm), shell=True)
 def move_to_public():
-	src = get_filepath()
-	dest = os.path.join(PUBLIC_DIR, os.path.join(PUBLIC_DIR, get_filename()))
-	os.rename(src, dest)
-	st = os.stat(dest)
-	os.chmod(dest, st.st_mode | stat.S_IROTH)
+	for file in get_output_files():
+		dest = os.path.join(PUBLIC_DIR, file)
+		file = os.path.join(OUTPUT_DIR, file)
+		os.rename(file, dest)
+		st = os.stat(dest)
+		os.chmod(dest, st.st_mode | stat.S_IROTH)
 
-def update_latest():
-	with open(os.path.join(PUBLIC_DIR, "latest.json"), 'w') as f:
-		json.dump(get_latest(), f)
+def generate_checksums():
+	for file in os.listdir(PUBLIC_DIR):
+		if 'md5' not in file:
+			file = os.path.join(PUBLIC_DIR, file)
+			md5 = subprocess.check_output("md5sum {}".format(file), shell=True).split()[0]
+			f = open(file + '.md5', 'w')
+			f.write(md5)
+			f.close()
 
-def get_latest():
-	md5 = subprocess.check_output("md5sum {}".format(get_filepath()), shell=True).split()[0]
-	return {
-		"filename": get_filename(),
-		"md5": md5
-	}
-
-def get_filename():
-	return os.listdir(OUTPUT_DIR)[0]
-
-def get_filepath():
-	filename = os.listdir(OUTPUT_DIR)[0]
-	return os.path.join(OUTPUT_DIR, filename)
+def get_output_files():
+	return os.listdir(OUTPUT_DIR)
 
 def cleanup():
 	os.rmdir(OUTPUT_DIR)
